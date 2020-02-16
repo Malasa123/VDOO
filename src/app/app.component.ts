f5668692c2da5442c18f6fd5724b2dc6b46503ed
Malasa123 <israelmalasa02@gmail.com>
2020-02-16 16:44:29 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vdo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vdoo-app';
+}
